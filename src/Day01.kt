f46e81a99923f3362654e574e152ca1d023f5dea
Tim Hill <timhill@alum.mit.edu>
2022-12-01 21:38:53 -0600

    Refactor Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,29 +1,5 @@
 fun main() {
-    val input = readInts("Day01")
-    var max = 0
-    var curMax = 0
-    input.forEach {
-        if (it.isNotEmpty()) {
-            curMax += it[0]
-        } else {
-            if (curMax > max) {
-                max = curMax
-            }
-            curMax = 0
-        }
-    }
-    println(max)
-
-    curMax = 0
-    val elves = buildList<Int> {
-        input.forEach {
-            if (it.isNotEmpty()) {
-                curMax += it[0]
-            } else {
-                add(curMax)
-                curMax = 0
-            }
-        }
-    }
-    println(elves.sortedDescending().subList(0,3).sum())
+    val elves = readInts("Day01").splitOnNull().map(List<Int>::sum).sortedDescending()
+    println(elves.first())
+    println(elves.take(3).sum())
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -18,16 +18,34 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-fun readDoubles(name: String) = readInput(name)
+fun readDoubles(name: String) = readInput(name).map(String::toDoubleOrNull)
+
+fun readAllDoubles(name: String) = readInput(name)
     .map { line ->
         DOUBLE_PATTERN.findAll(line)
             .map { match -> match.value.toDouble() }
             .toList()
     }
 
-fun readInts(name: String) = readInput(name)
+fun readInts(name: String) = readInput(name).map(String::toIntOrNull)
+
+fun readAllInts(name: String) = readInput(name)
     .map { line ->
         INT_PATTERN.findAll(line)
             .map { match -> match.value.toInt() }
             .toList()
     }
+
+fun <T> Iterable<T>.split(
+    predicate: (T) -> Boolean
+): List<List<T>> = fold(listOf(listOf())) { result, element ->
+    if (predicate(element)) {
+        result + listOf(listOf())
+    } else {
+        result.dropLast(1) + listOf(result.last() + element)
+    }
+}
+
+fun isNull(any: Any?) = any == null
+
+fun <T> Iterable<T?>.splitOnNull() = split(::isNull).map(List<T?>::filterNotNull)
