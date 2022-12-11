5a8fce1adf0272afa6d1f07666287cb04f1f90a4
Tim Hill <timhill@alum.mit.edu>
2022-12-10 22:11:10 -0600

    Day 8 refactor

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,60 +1,48 @@
-import kotlin.math.max
+fun Iterable<Int>.product() = fold(1) { acc, elt -> acc * elt }
+inline fun <T> Iterable<T>.productOf(selector: (T) -> Int) = fold(1) { acc, elt -> acc * selector(elt) }
+fun <T : List<U>, U> Iterable<T>.column(index: Int) = map { it[index] }
+inline fun <T : List<U>, U> Iterable<T>.columnOrElse(index: Int, defaultValue: (Int) -> U) =
+    map { it.getOrElse(index, defaultValue) }
 
-private fun Iterable<Int>.maxOrNeg() = maxOrNull() ?: -1
+private data class VantagePoint(
+    val height: Int,
+    val north: List<Int>,
+    val east: List<Int>,
+    val south: List<Int>,
+    val west: List<Int>,
+) {
+    val directions = listOf(north, east, south, west)
+    val isVisible = directions.any { it.isEmpty() || it.max() < height }
 
-private typealias TreeGrid = List<List<Int>>
-
-private fun TreeGrid.column(col: Int) = map { it[col] }
-
-private fun TreeGrid.isVisible(rowIndex: Int, columnIndex: Int): Boolean {
-    val row = get(rowIndex)
-    val column = column(columnIndex)
-    val height = row[columnIndex]
-    return (row.take(columnIndex).maxOrNeg() < height) or
-            (row.drop(columnIndex + 1).maxOrNeg() < height) or
-            (column.take(rowIndex).maxOrNeg() < height) or
-            (column.drop(rowIndex + 1).maxOrNeg() < height)
-}
-
-private fun List<Int>.viewDistance(height: Int): Int {
-    val idx = indexOfFirst { it >= height }
-    return if (idx == -1) {
-        size
-    } else {
-        idx + 1
+    private fun viewDistance(treeLine: List<Int>): Int {
+        val idx = treeLine.indexOfFirst { it >= height }
+        return if (idx == -1) {
+            treeLine.size
+        } else {
+            idx + 1
+        }
     }
-}
 
-private fun TreeGrid.scenicScore(rowIndex: Int, columnIndex: Int): Int {
-    val row = get(rowIndex)
-    val column = column(columnIndex)
-    val height = row[columnIndex]
-    return row.take(columnIndex).asReversed().viewDistance(height) *
-            row.drop(columnIndex + 1).viewDistance(height) *
-            column.take(rowIndex).asReversed().viewDistance(height) *
-            column.drop(rowIndex + 1).viewDistance(height)
+    val scenicScore = directions.productOf(::viewDistance)
 }
 
 fun main() {
     val trees = readInput("Day08").map {
         it.map(Char::digitToInt)
     }
-    var sum = 0
-    for (i in trees.indices) {
-        for (j in trees[i].indices) {
-            if (trees.isVisible(i, j)) {
-                sum += 1
+    val vantagePoints = buildList {
+        trees.forEachIndexed { i, row ->
+            row.forEachIndexed { j, height ->
+                val column = trees.column(j)
+                val north = column.take(i).reversed()
+                val east = row.drop(j + 1)
+                val south = column.drop(i + 1)
+                val west = row.take(j).reversed()
+                add(VantagePoint(height, north, east, south, west))
             }
         }
     }
-    println(sum)
 
-    var bestScore = 0
-    for (i in trees.indices) {
-        for (j in trees[i].indices) {
-            val score = trees.scenicScore(i, j)
-            bestScore = max(bestScore, score)
-        }
-    }
-    println(bestScore)
+    println(vantagePoints.count(VantagePoint::isVisible))
+    println(vantagePoints.maxOf(VantagePoint::scenicScore))
 }
